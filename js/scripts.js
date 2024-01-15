3acebc0186e3ac82d2f0aade9fadbcc66eb3a2fd
Yufan <yuxu1@users.noreply.github.com>
2024-01-15 15:23:22 -0800

    removed testing code, created array of Pokemon,added first 9 Pokemon to array list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-alert('Hello world');
-let favoriteFood='spicy noodles';
-document.write(favoriteFood);
+let pokemonList=[
+    {name:'Bulbasaur',height:0.7,weight:6.9,types:['grass','poison']},
+    {name:'Ivysaur',height:1,weight:13,types:['grass','poision']},
+    {name:'Venusaur',height:2,weight:100,types:['grass','poison']},
+    {name:'Charmander',height:0.6,weight:8.5,types:'fire'},
+    {name:'Charmelion',height:1.1,weight:19,types:'fire'},
+    {name:'Charizard',height:1.7,weight:90.5,types:['fire','flying']},
+    {name:'Squirtle',height:0.5,weight:9,types:'water'},
+    {name:'Wartortle',height:1,weight:22.5,types:'water'},
+    {name:'Blastoise',height:1.6,weight:85.5,types:'water'}
+];
+console.log(pokemonList);
