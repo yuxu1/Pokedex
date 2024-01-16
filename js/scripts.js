7b1216c4d5554efb95f17aeb56df90b1618e042b
Yufan <yuxu1@users.noreply.github.com>
2024-01-15 17:17:25 -0800

    added for loop+conditional to print each Pokemon's name and height and make note for specially tall Pokemon, edited height/weight to floating numbers for consistency

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
 let pokemonList=[
     {name:'Bulbasaur',height:0.7,weight:6.9,types:['grass','poison']},
-    {name:'Ivysaur',height:1,weight:13,types:['grass','poision']},
-    {name:'Venusaur',height:2,weight:100,types:['grass','poison']},
+    {name:'Ivysaur',height:1.0,weight:13.0,types:['grass','poision']},
+    {name:'Venusaur',height:2.0,weight:100.0,types:['grass','poison']},
     {name:'Charmander',height:0.6,weight:8.5,types:'fire'},
-    {name:'Charmelion',height:1.1,weight:19,types:'fire'},
+    {name:'Charmeleon',height:1.1,weight:19.0,types:'fire'},
     {name:'Charizard',height:1.7,weight:90.5,types:['fire','flying']},
-    {name:'Squirtle',height:0.5,weight:9,types:'water'},
-    {name:'Wartortle',height:1,weight:22.5,types:'water'},
+    {name:'Squirtle',height:0.5,weight:9.0,types:'water'},
+    {name:'Wartortle',height:1.0,weight:22.5,types:'water'},
     {name:'Blastoise',height:1.6,weight:85.5,types:'water'}
 ];
 console.log(pokemonList);
+
+//prints on screen all the Pokemon's names and height from the list
+//if height is over 2(m),add note to highlight special Pokemon
+for(i=0;i<pokemonList.length;i++){
+    if(pokemonList[i].height>1.7){
+        document.write(pokemonList[i].name+' (height:'+pokemonList[i].height+') - Wow, that\'s big!'+'<br>')
+    } else{
+        document.write(pokemonList[i].name+' (height:'+pokemonList[i].height+')'+'<br>');
+    }
+}
