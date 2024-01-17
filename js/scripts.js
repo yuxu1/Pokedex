b0f59f92f2ceca52935e871d12c33d09ac372de8
Yufan <yuxu1@users.noreply.github.com>
2024-01-16 20:05:37 -0800

    wrapped pokemonList in an IIFE and updated forEach loop to access the array from inside IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
-let pokemonList=[
-    {name:'Bulbasaur',height:0.7,weight:6.9,types:['grass','poison']},
-    {name:'Ivysaur',height:1.0,weight:13.0,types:['grass','poision']},
-    {name:'Venusaur',height:2.0,weight:100.0,types:['grass','poison']},
-    {name:'Charmander',height:0.6,weight:8.5,types:'fire'},
-    {name:'Charmeleon',height:1.1,weight:19.0,types:'fire'},
-    {name:'Charizard',height:1.7,weight:90.5,types:['fire','flying']},
-    {name:'Squirtle',height:0.5,weight:9.0,types:'water'},
-    {name:'Wartortle',height:1.0,weight:22.5,types:'water'},
-    {name:'Blastoise',height:1.6,weight:85.5,types:'water'}
-];
-console.log(pokemonList);
+//wrap list of Pokemon in an IIFE
+let pokemonRepository = (function(){
+    //list of Pokemon
+    let pokemonList=[
+        {name:'Bulbasaur',height:0.7,weight:6.9,types:['grass','poison']},
+        {name:'Ivysaur',height:1.0,weight:13.0,types:['grass','poision']},
+        {name:'Venusaur',height:2.0,weight:100.0,types:['grass','poison']},
+        {name:'Charmander',height:0.6,weight:8.5,types:'fire'},
+        {name:'Charmeleon',height:1.1,weight:19.0,types:'fire'},
+        {name:'Charizard',height:1.7,weight:90.5,types:['fire','flying']},
+        {name:'Squirtle',height:0.5,weight:9.0,types:'water'},
+        {name:'Wartortle',height:1.0,weight:22.5,types:'water'},
+        {name:'Blastoise',height:1.6,weight:85.5,types:'water'}
+    ];
+    //retrieve whole list of Pokemon
+    function getAll(){
+        return pokemonList;
+    }
+    //add Pokemon entry to list
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+    //return functions as keys to object pokemonRepository
+    return {
+        getAll:getAll,
+        add:add
+    };
+})();
 
 //prints on screen all the Pokemon's names and height from the list
 //if height is over 1.7(m),add note to highlight special Pokemon
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
     if(pokemon.height>1.7){
         document.write('<p>'+pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!'+'</p>')
     }else{
         document.write('<p>'+pokemon.name + ' (height: ' + pokemon.height+')'+'</p>')
     }
 });
+
