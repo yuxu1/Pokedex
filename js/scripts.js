a69f4319e30b61434be4665ba783fa5567576e65
Yufan <yuxu1@users.noreply.github.com>
2024-01-17 21:25:54 -0800

    edited add function to only add pokemon that meet the formatting; added addListItem function to add list item as a button to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,67 @@
 //wrap list of Pokemon in an IIFE
-let pokemonRepository = (function(){
+let pokemonRepository = (function () {
     //list of Pokemon
-    let pokemonList=[
-        {name:'Bulbasaur',height:0.7,weight:6.9,types:['grass','poison']},
-        {name:'Ivysaur',height:1.0,weight:13.0,types:['grass','poision']},
-        {name:'Venusaur',height:2.0,weight:100.0,types:['grass','poison']},
-        {name:'Charmander',height:0.6,weight:8.5,types:'fire'},
-        {name:'Charmeleon',height:1.1,weight:19.0,types:'fire'},
-        {name:'Charizard',height:1.7,weight:90.5,types:['fire','flying']},
-        {name:'Squirtle',height:0.5,weight:9.0,types:'water'},
-        {name:'Wartortle',height:1.0,weight:22.5,types:'water'},
-        {name:'Blastoise',height:1.6,weight:85.5,types:'water'}
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, weight: 6.9, types: ['grass', 'poison'] },
+        { name: 'Ivysaur', height: 1.0, weight: 13.0, types: ['grass', 'poision'] },
+        { name: 'Venusaur', height: 2.0, weight: 100.0, types: ['grass', 'poison'] },
+        { name: 'Charmander', height: 0.6, weight: 8.5, types: 'fire' },
+        { name: 'Charmeleon', height: 1.1, weight: 19.0, types: 'fire' },
+        { name: 'Charizard', height: 1.7, weight: 90.5, types: ['fire', 'flying'] },
+        { name: 'Squirtle', height: 0.5, weight: 9.0, types: 'water' },
+        { name: 'Wartortle', height: 1.0, weight: 22.5, types: 'water' },
+        { name: 'Blastoise', height: 1.6, weight: 85.5, types: 'water' }
     ];
+
     //retrieve whole list of Pokemon
-    function getAll(){
+    function getAll() {
         return pokemonList;
     }
-    //add Pokemon entry to list
-    function add(pokemon){
-        pokemonList.push(pokemon);
+
+    //add Pokemon entry to list when the data meets the format
+    function add(pokemon) {
+        if (
+            typeof pokemon === 'object' &&
+            'name' in pokemon &&
+            'height' in pokemon &&
+            'weight' in pokemon &&
+            'types' in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log('pokemon data invalid or incomplete');
+        }
     }
+
+    //function to add a button for each addition to the list of Pokemon
+    function addListItem(pokemon) {
+        let listedEntries = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        //create button element for each Pokemon entry
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('list-entry');
+        //appends button to list item as its child
+        listItem.appendChild(button);
+        //appends list item to unodered list as its child
+        listedEntries.appendChild(listItem);
+        //add event listener to button created-hear for 'click' and show details of selected Pokemon
+        button.addEventListener('click',showDetails);
+    }
+
+    function showDetails(pokemon){
+        console.log(pokemon.name);
+    }
+
     //return functions as keys to object pokemonRepository
     return {
-        getAll:getAll,
-        add:add
+        getAll: getAll,
+        add: add,
+        addListItem: addListItem
     };
 })();
 
-//prints on screen all the Pokemon's names and height from the list
-//if height is over 1.7(m),add note to highlight special Pokemon
-pokemonRepository.getAll().forEach(function(pokemon){
-    if(pokemon.height>1.7){
-        document.write('<p>'+pokemon.name + ' (height: ' + pokemon.height + ') - Wow, that\'s big!'+'</p>')
-    }else{
-        document.write('<p>'+pokemon.name + ' (height: ' + pokemon.height+')'+'</p>')
-    }
+//add each pokemon in pokemonRepository into <ul> .pokemon-list
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
-
